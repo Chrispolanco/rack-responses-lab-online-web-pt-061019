9b973d488ec60c961f689bc66758ec5bd4e3822f
Christian Polanco <chrispolanco@sbcglobal.net>
2019-09-10 04:18:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
   
   def call(env)
     resp = Rack::Response.new 
-    time_now = Time
+    time_now = Time.now
     
     if time_now.hour>=12
       resp.write "Good Afternoon"
